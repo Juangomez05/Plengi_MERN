b1152b367984185a5e1b1c61b4db265a5ed3f824
Juangomez05 <juanpgs1201@gmail.com>
2023-07-11 13:54:25 -0500

    Apu_id material, equipo y mano de obra

--- front_plengi/src/components/Apu_user_card.jsx
+++ front_plengi/src/components/Apu_user_card.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Apu_user_card( {apu} ) {
 
     return (
-        <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+        <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md ">
             <header className="flex justify-between">
                 <h1 className="text-2xl font-bold">{apu.name_apu}</h1>
             </header>
--- front_plengi/src/pages/ApuFormPage.jsx
+++ front_plengi/src/pages/ApuFormPage.jsx
@@ -5,17 +5,29 @@ import { useApu } from "../context/Apu_user_context";
 import { useParams, useNavigate } from "react-router-dom";
 
 function ApuFormPage() {
-  const { materials, getMaterials_BD } = useBD();
+  const { 
+    materials, 
+    getMaterials_BD,
+    equipos,
+    getEquipos_BD,
+    labours,
+    getLabours_BD
+  } = useBD();
   const { getApu, updateApu } = useApu();
 
   const [selectedMaterial, setSelectedMaterial] = useState(null);
+  const [selectedEquipo, setSelectedEquipo] = useState(null)
+  const [selectedLabour, setSelectedLabour] = useState(null)
   const [apuData, setApuData] = useState(null);
+  const [nameApu, setNameApu] = useState(null)
 
   const navigate = useNavigate();
   const params = useParams();
 
   useEffect(() => {
     getMaterials_BD();
+    getEquipos_BD();
+    getLabours_BD();
 
     if (params.id) {
       loadApu();
@@ -27,6 +39,9 @@ function ApuFormPage() {
       const apu = await getApu(params.id);
       setApuData(apu);
       setSelectedMaterial(apu.material);
+      setSelectedEquipo(apu.equipo)
+      setSelectedLabour(apu.labour)
+      setNameApu(apu.name_apu)
     } catch (error) {
       console.log(error);
     }
@@ -38,30 +53,68 @@ function ApuFormPage() {
     setSelectedMaterial(material);
   };
 
-  const  handleSubmit  = async () => {
+  const handleSelectChangeEquipo = (e) => {
+    const selectedEquipoId = e.target.value
+    const equipo = equipos.find((m) => m._id === selectedEquipoId)
+    setSelectedEquipo(equipo) 
+  }
+
+  const handleSelectChangeLabour = (e) => {
+    const selectedLabourId = e.target.value
+    const labour = labours.find((m) => m._id === selectedLabourId)
+    setSelectedLabour(labour)
+  }
+
+  const handleSubmit = async () => {
     try {
       if (params.id) {
-        
-        if (!apuData.materiales_id.includes(selectedMaterial._id) ) {
+        if (!apuData.materiales_id.includes(selectedMaterial._id)) {
           apuData.materiales_id.push(selectedMaterial._id)
         }
-
         const actuliazado = await updateApu(params.id, apuData);
-
-        console.log(actuliazado);
-      }else{
+        console.log(apuData.materiales_id);
+      } else {
         console.log("No tiene params");
       }
-
     } catch (error) {
       console.log(error);
     }
   };
 
+  const handleSubmitEquipo = async () => {
+    try {
+      if (params.id) {
+        if (!apuData.equipos_id.includes(selectedEquipo._id)) {
+          apuData.equipos_id.push(selectedEquipo._id)
+        }
+        const actuliazado = await updateApu(params.id, apuData)
+        console.log(actuliazado);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleSubmitLabour = async () => {
+    try {
+      if(params.id){
+        if (!apuData.labours_id.includes(selectedLabour._id)) {
+          apuData.labours_id.push(selectedLabour._id)
+        }
+        const actuliazado = await updateApu(params.id, apuData)
+        console.log(actuliazado);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
 
   return (
     <div>
-      <h1>Apu {params.id}</h1>
+
+      {/* MATERIALES */}
+      <h1>Apu {nameApu}</h1>
       <label htmlFor="materialSelect">Selecciona un material:</label>
       <select
         className="text-black"
@@ -77,6 +130,42 @@ function ApuFormPage() {
       </select>
       <button onClick={handleSubmit}>
         Agregar Material
+      </button>
+
+      {/* EQUIPOS */}
+      <label className="px-4 py-2" htmlFor="equipoSelect">Selecciona un equipo:</label>
+      <select
+        className="text-black"
+        id="equipoSelect"
+        onChange={handleSelectChangeEquipo}
+      >
+        <option value="">Selecciona un equipo</option>
+        {equipos.map((equipo) => (
+          <option key={equipo._id} value={equipo._id}>
+            {equipo.name}
+          </option>
+        ))}
+      </select>
+      <button onClick={handleSubmitEquipo}>
+        Agregar Equipo
+      </button>
+
+      {/* MANO DE OBRA */}
+      <label className="px-4 py-2" htmlFor="labourSelect">Selecciona la mano de obra:</label>
+      <select
+        className="text-black"
+        id="labourSelect"
+        onChange={handleSelectChangeLabour}
+      >
+        <option value="">Selecciona la mano de obra</option>
+        {labours.map((labour) => (
+          <option key={labour._id} value={labour._id}>
+            {labour.name}
+          </option>
+        ))}
+      </select>
+      <button onClick={handleSubmitLabour}>
+        Agregar Mano de obra
       </button>
 
     </div>
